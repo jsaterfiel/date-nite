6905c026dfbae632519231222275cbfb14eddb4e
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-04-23 17:34:34 -0400

    break down component into smaller components

--- src/routes/CreateDate.js
+++ src/routes/CreateDate.js
@@ -1,52 +1,14 @@
 import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
-import Moment from 'moment';
-
-import 'react-datepicker/dist/react-datepicker.css';
-import '../styles/create_date.css';
-
-const COUNT = [2,3,4,5,6,7,8];
+import CreateDateHeader from '../components/create_date_header';
 
 class CreateDate extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            currentDate: Moment(),
-            selectedDate: null
-        };
-    }
-
-    countOptions = () => {
-        return COUNT.map(option => {
-            return <span key={option} className="dropdown-item">{ option }</span>;
-        });
-    };
-
-    onSelectDateChange = (date) => {
-        this.setState({
-            selectedDate: date
-        });
-    };
-
     render () {
         return (
             <div className="create-date-page">
-                <nav className="create-date-nav navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div className="input-group mb-3 count-container">
-                        <div className="input-group-prepend">
-                            <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Count</button>
-                            <div className="dropdown-menu">
-                                {this.countOptions()}
-                            </div>
-                        </div>
-                        <input type="text" className="form-control count-text" aria-label="Text input with dropdown button" />
-                    </div>
-                    <DatePicker className="custom-datepicker" 
-                    minDate={this.state.currentDate} selected={this.state.selectedDate} 
-                    onChange={this.onSelectDateChange} placeholderText="When's your date?" />
-                </nav>
-                MAP GOES HERE!
+                <CreateDateHeader />
+                <div className="map-container">
+                    MAP GOES HERE!
+                </div>
             </div>
         );
     }
