9d81b4261b1ff26f2c05cbd5d9c2675e7cabdcdb
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-05-10 18:58:23 -0400

    revert certain local changes before commit to master

--- api/services/cache.js
+++ api/services/cache.js
@@ -4,6 +4,6 @@ const bluebird = require('bluebird')
 bluebird.promisifyAll(redis.RedisClient.prototype)
 bluebird.promisifyAll(redis.Multi.prototype)
 
-// const cache = redis.createClient({url: 'redis://redis:6379'})
-const cache = {}
+const cache = redis.createClient({url: 'redis://redis:6379'})
+
 module.exports = cache
