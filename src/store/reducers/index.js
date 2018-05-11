d9ac95d74df960e6674aa0fc135915943c8a55c7
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-05-10 20:37:22 -0400

    extend redux for entire create date

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import general from './general'
 import YelpInfo from './reducer_yelp'
+import DateAndCount from './reducer_date'
 
 export default combineReducers({
   router: routerReducer,
   restaurantDetails: YelpInfo,
+  dateAndCount: DateAndCount,
   general
 })
